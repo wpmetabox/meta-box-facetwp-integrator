7d68ae40d227a5afd336172e7c58072f3b6fa0b5
Andrew Minion <andrew.minion@luminfire.com>
2023-01-03 17:21:45 -0600

    add indexing for post relationships

--- meta-box-facetwp-integrator.php
+++ meta-box-facetwp-integrator.php
@@ -15,3 +15,18 @@ if ( ! class_exists( 'MB_FacetWP_Integrator' ) ) {
 	require __DIR__ . '/class-mb-facetwp-integrator.php';
 	new MB_FacetWP_Integrator;
 }
+
+add_action(
+	'mb_relationships_init',
+	function () {
+		add_filter(
+			'facetwp_facet_types',
+			function( $types ) {
+				require_once __DIR__ . '/class-mb-relationships-facetwp.php';
+
+				$types[ MB_Relationships_FacetWP::FACET_TYPE ] = new MB_Relationships_FacetWP();
+				return $types;
+			}
+		);
+	}
+);
